cd63f1f488c06489f266d63d45765742fdbdd1d7
Jed Peek <jedpeek@yahoo.com>
2018-06-05 15:20:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
 def prime?(int)
-  if int == 0 || int == 1 || int == 2
+  if int == 2..3
     return true
+  elsif int <= 1
+    return false
   end
+
+# range = (3...(int/2).round).to_a
+  return false if int.even?
+
 end
