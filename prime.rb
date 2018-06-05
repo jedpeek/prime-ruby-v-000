19b6626463871e13cbfb7dc3bc0a69de4c15568f
Jed Peek <jedpeek@yahoo.com>
2018-06-05 15:26:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -10,8 +10,7 @@ def prime?(int)
   range = (3..(int/2).round).to_a
   i = 0
   while i < range.length
-  return false if int % range[i] == 0
-  i += 1
+  return false if int % range[i] == 0, i += 1
   end
     return true
 end
